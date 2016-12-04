dce86817e05b9e6ee79543eaaad9943f2dbb2283
panming <panming@xiyou3g.com>
2016-12-04 22:05:54 +0800

    m

---
+++ src/components/header.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="header-container">
+		<div class="logo">LOGO</div>
+		<div class="header-right">
+			<div class="space-nav">
+				西安邮电大学 > 在线评测平台
+			</div>
+		</div>
+	</div>
+</template>
+<script type="text/javascript">
+	export default {
+
+	}
+</script>
+
+<style type="text/css">
+	.header-container {
+		/*height: 50px;*/
+		/*line-height: 50px;*/
+		width: 100%;
+		/*float: left;*/
+		overflow: hidden;
+	}
+	.logo {
+		float: left;
+		line-height: 50px;
+		width: 180px;
+		background-color: #3c375a;
+		color: white;
+		text-align: center;
+	}
+	.header-right {
+		margin-left: 180px;
+		color: #333;
+		line-height: 49px;
+		border-bottom: 1px #ccc solid;
+		/*float: left;*/
+		/*background-color: #ccc;*/
+	}
+	.space-nav {
+		color: #777;
+		margin-left: 10px;
+	}
+	.space-nav::before{
+		content: '';
+		width: 20px;
+		height: 20px;
+		background-image: url('../assets/school.png');
+	}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,19 @@
 <template>
+<div class="body">
+	<Header></Header>
   <p>
     index
   </p>
+</div>
+
 </template>
+<script type="text/javascript">
+import Header from '../components/header'
+
+	export default {
+		components: {
+			Header
+		}
+	}
+</script>
 
