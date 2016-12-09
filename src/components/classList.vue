16e935483e938bdb73773d67ad8356a845199cb1
Pandapm <panda_m@126.com>
2016-12-09 21:31:39 +0800

    add classList component

---
+++ src/components/classList.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="class-list">
+		<ul class="class-list-ul">
+			<li v-for="item in classInfo">
+				
+			</li>
+		</ul>
+	</div>
+</template>
+<script>
+	export default {
+		props: ['classInfo'],
+
+	}
+</script>
+<style>
+	
+</style>
