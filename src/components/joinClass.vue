c9b0d71b3033f1a25cc438379a6cbb05df1de603
Pandapm <panda_m@126.com>
2016-12-12 21:38:41 +0800

    :sparkles: add join component

---
+++ src/components/joinClass.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="join-container">
+		<div class="join-content">
+			<h2>加入班级</h2>
+			<p>输入要加入班级的编号，点击「查询」按钮，核实班级信息无误后点击「申请加入」按钮就行。</p>
+			<p>申请发送后，该班级的管理员选择「同意」加入，则你会成为该班级的成员。</p>
+			<p>如果班级属性设置为「允许任何人加入该班」，则无需班级管理员同意就可以「直接加入」该班级。</p>
+			<div class="join-search">
+				<input type="text" class="input-large" name="" placeholder="请输入班级编号" />
+				<button class="btn btn-primary" name="">找一找</button>
+				<router-link class='join-return' to="/class">返回</router-link>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+
+	}
+</script>
+<style lang="less">
+	.join-container {
+		text-align: center;
+		width: 100vw;
+		height: 100vh;
+		position: fixed;
+		top: 0;
+		left: 0;
+		background-color: rgba(0,0,0,0.5);
+		padding: 50px;
+		.join-content {
+			background-color: rgba(0,0,0,0.4);
+			padding: 40px;
+			width: 700px;
+			text-align: left;
+			margin: 50px auto;
+			color: white;
+			.join-search {
+				margin-top: 20px;
+				input[type='text'] {
+					width: 400px;
+					height: 32px;
+					border: none;
+					border-radius: 5px;
+					line-height: 32px;
+					color: #444;
+					font-size: 18px;
+					padding-left: 10px;
+					margin-right: 10px;
+				}
+				button {
+					width: 100px;
+				}
+				.join-return {
+					margin-left: 10px;
+					font-size: 15px;
+					display: inline-block;
+					line-height: 15px;
+				}
+			}
+		}
+	}
+</style>
