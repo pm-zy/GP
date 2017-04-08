8ec2c975a07653deb9eb02a4c13f9d834b995b30
Pandapm <panda_m@126.com>
2017-04-08 23:38:49 +0800

    m

--- src/pages/user.vue
+++ src/pages/user.vue
@@ -1,7 +1,18 @@
 <template>
 	<div >
 		<TopBar path='我的空间'></TopBar>
-		<div class="body"></div>
+		<div class="body">
+			<div class="user-container">
+				<div class="pic">
+					<img src="../assets/logo.png" alt="">
+				</div>
+				<div class="user-info">
+					<h2>{{nickname}}</h2>
+					<p class="org">{{org}}</p>
+					<p v-if="userType == 1" class="student">学生</p>
+				</div>
+			</div>
+		</div>
 	</div>
 
 </template>
@@ -12,7 +23,12 @@ import Vue from 'vue';
 	export default {
 		data() {
 			return {
-				
+				nickname: 'PM',
+				email: 'i@varpm.com',
+				telNum: '18829295436',
+				personId: '0001',
+				org: '西安邮电大学 计算机学院 计科1303',
+				userType: 1
 			}
 		},
 		components: {
@@ -20,4 +36,38 @@ import Vue from 'vue';
 		}
 	}
 </script>
+<style lang="less">
+	.user-container {
+		width: 80%;
+		margin: 20px auto;
+		overflow: hidden;
+		.user-info  {
+			text-align: center;
+			h2 {
 
+			}
+			.org {
+				color: #888;
+				font-size: 16px;
+			}
+			.student {
+				font-size: 18px;
+				color: #abcdef;
+				font-weight: bold;
+
+			}
+		}
+		.pic {
+			width: 100px;
+			height: 100px;
+			margin: 10px auto;
+			overflow: hidden;
+			background-color: #fff;
+			border-radius: 10px;
+			img {
+				width: 100px;
+				height: 100px;
+			}
+		}
+	}
+</style>
