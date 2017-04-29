99a2338efa2a17a5b05f29927855192e934ce8a3
panming <mingpan@sohu-inc.com>
2017-04-29 22:35:10 +0800

    add info page

---
+++ src/pages/info.vue
@@ -0,0 +1,26 @@
+<template>
+	<div >
+		<TopBar path='资料库'></TopBar>
+		<router-view></router-view>
+	</div>
+
+</template>
+<script>
+import TopBar from '../components/topBar'
+import Vue from 'vue';
+import store from '../vuex/store'
+
+export default {
+	components: {
+		TopBar
+	},
+	data() {
+		if(!store.getters.getLoginStatus.status) {
+			alert('请先登录');
+			console.log(store.getters.getLoginStatus.status);
+			this.$router.replace('/login');
+		}
+		return {}
+	}
+}
+</script>
